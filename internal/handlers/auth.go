ac29b03669fe1a064e7ac49087255253886ffdc4
Pieter Ernst Vermeulen <contact@pevermeulen.com>
2026-08-24 00:34:24 -0200

    Change register show handler to private

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -6,7 +6,7 @@ import (
 	"rqpt/hadida/internal/templates"
 )
 
-func (h *Handler) ShowRegister() http.HandlerFunc {
+func (h *Handler) showRegister() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		component := templates.Register(templates.RegisterProps{})
 		component.Render(r.Context(), w)
